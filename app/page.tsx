12ae676aa98fcbf2f61ed665d6f743b988b248ca
kingsedu <chinedumeh26@yahoo.com>
2026-01-08 11:20:01 +0100

    add logo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
+import LogoAnimation from "@/components/LogoAnimation";
+import Portfolio from "@/components/Portfolio";
 export default function Home() {
   return (
     <>
       <Hero />
+      <LogoAnimation />
       <About />
+      <Portfolio />
     </>
   );
 }
